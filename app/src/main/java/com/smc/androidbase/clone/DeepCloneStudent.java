ea18f19b0bb22e50ab727d183adc8893fee2ff1d
沈梦超 <769193938@qq.com>
2018-08-27 15:40:28 +0800

    深拷贝和浅拷贝

--- app/src/main/java/com/smc/androidbase/clone/DeepCloneStudent.java
+++ app/src/main/java/com/smc/androidbase/clone/DeepCloneStudent.java
@@ -4,27 +4,34 @@ package com.smc.androidbase.clone;
  * @author shenmengchao
  * @version 1.0.0
  * @date 2018/8/27
- * @description 这里是浅拷贝，也就是说，只会拷贝对象中的基础类型，对象中的引用类型，是直接把引用拷贝过去（而没有去把对象中引用类型重新new一下）
- * 所以在例子中，改变了student2.professor.name = "beta"后，student1.professor.name 的值也会变成"beta"
+ * @description 深拷贝，需要自己重写clone方法，在clone()方法中自己确定哪些类需要重新new
+ *
  */
 
-public class LightCloneStudent implements Cloneable {
+public class DeepCloneStudent implements Cloneable {
 
     String name;
     int age;
     Professor professor;
 
-    public LightCloneStudent(String name, int age, Professor professor) {
+    public DeepCloneStudent(String name, int age, Professor professor) {
         this.name = name;
         this.age = age;
         this.professor = professor;
     }
 
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+         DeepCloneStudent o = (DeepCloneStudent) super.clone();
+         o.professor = (Professor) professor.clone();
+         return o;
+    }
+
     public static void main(String[] args) throws CloneNotSupportedException {
         Professor professor1 = new Professor();
         professor1.name = "alpha";
-        LightCloneStudent student1 = new LightCloneStudent("xiaoming", 12, professor1);
-        LightCloneStudent student2 = (LightCloneStudent) student1.clone();
+        DeepCloneStudent student1 = new DeepCloneStudent("xiaoming", 12, professor1);
+        DeepCloneStudent student2 = (DeepCloneStudent) student1.clone();
         student2.professor.name = "beta";
 
         System.out.println("student1.p = " + student1.professor.name);
@@ -32,8 +39,13 @@ public class LightCloneStudent implements Cloneable {
     }
 
 
-    public static class Professor {
+    public static class Professor implements Cloneable{
         String name;
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
     }
 
 }
--- app/src/main/java/com/smc/androidbase/clone/LightCloneStudent.java
+++ app/src/main/java/com/smc/androidbase/clone/LightCloneStudent.java
@@ -4,12 +4,41 @@ package com.smc.androidbase.clone;
  * @author shenmengchao
  * @version 1.0.0
  * @date 2018/8/27
- * @description
+ * @description 这里是浅拷贝，也就是说，只会拷贝对象中的基础类型，对象中的引用类型，是直接把引用拷贝过去（而没有去把对象中引用类型重新new一下）
+ * 所以在例子中，改变了student2.professor.name = "beta"后，student1.professor.name 的值也会变成"beta"
  */
 
-public class Student implements Cloneable {
+public class LightCloneStudent implements Cloneable {
 
+    String name;
+    int age;
+    Professor professor;
+
+    public LightCloneStudent(String name, int age, Professor professor) {
+        this.name = name;
+        this.age = age;
+        this.professor = professor;
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Professor professor1 = new Professor();
+        professor1.name = "alpha";
+        LightCloneStudent student1 = new LightCloneStudent("xiaoming", 12, professor1);
+        LightCloneStudent student2 = (LightCloneStudent) student1.clone();
+        student2.professor.name = "beta";
+
+        System.out.println("student1.p = " + student1.professor.name);
+        System.out.println("student2.p = " + student2.professor.name);
+    }
+
+
+    public static class Professor {
+        String name;
+    }
 
 }
+
+
+
 
 
